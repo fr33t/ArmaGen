afd0e0e35cfd7fdee0ed9251d630e88c76f9d314
fb0sh <fb0sh@outlook.com>
2024-11-09 11:38:19 +0800

    fix(submodule)

--- loaders/xor_loader_fb0sh/src/bin/loader.rs
+++
@@ -1,7 +0,0 @@
-use armagen::{Obfuscation, O};
-
-fn main() {
-    // 条件编译 lib.rs
-    // file read // wget ? O.load();?
-    O.exec();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use ArmaGen::*;
+
 fn main() {
     work(init());
 }
